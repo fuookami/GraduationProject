ea05fac0eec3ca0fcb876d22f65d64ebff0b81b7
fuookami <fuookami@163.com>
2018-03-19 22:45:36 +0800

    定义文件后缀

--- VisualExperimentalDesignAids/VEDAGlobal.cpp
+++ VisualExperimentalDesignAids/VEDAGlobal.cpp
@@ -3,5 +3,9 @@
 namespace VEDA
 {
 	const std::string GUIEntrance("file:///WebRoot/main.html");
-	const std::string ProjectFileSuffix("veda");
+	const std::string ProjectFileSuffix("vedaproj");
+	const std::string ProcessFileSuffix("vedaproc");
+	const std::string OperationFileSuffix("vedaops");
+	const std::string DataModalDefinitionFileSuffix("veda.xsd");
+	const std::string DataFileSuffix("veda.xml");
 };
--- VisualExperimentalDesignAids/VEDAGlobal.h
+++ VisualExperimentalDesignAids/VEDAGlobal.h
@@ -6,4 +6,8 @@ namespace VEDA
 {
 	extern const std::string GUIEntrance;
 	extern const std::string ProjectFileSuffix;
+	extern const std::string ProcessFileSuffix;
+	extern const std::string OperationFileSuffix;
+	extern const std::string DataModalDefinitionFileSuffix;
+	extern const std::string DataFileSuffix;
 };
