adfa7f5d8db62eda03e8c70729450f4da8796cf2
fuookami <fuookami@163.com>
2018-04-18 01:22:40 +0800

    重组公共模块文件工具集

--- PublicModule/FileUtils.cpp
+++ PublicModule/FileUtils.cpp
@@ -5,227 +5,238 @@
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/convenience.hpp>
 #include <sstream>
-#include <fstream>
-#include <algorithm>
 
-namespace FileUtils
+namespace SSUtils
 {
-	const std::string & initailPath()
+	namespace File
 	{
-		static const std::string initailPath(boost::filesystem::initial_path().string());
-		return initailPath;
-	}
+#ifdef _WIN32
+		const std::string PathSeperator("\\");
+#else
+		const std::string PathSeperator("/");
+#endif
+		const std::string ExtensionSeperator(".");
+		const std::string InitailPath(boost::filesystem::initial_path().string());
 
-	const bool checkFileExist(const std::string & targetUrl)
-	{
-		std::ifstream fin(targetUrl);
-		if (!fin.is_open())
-		{
-			return false;
-		}
-
-		fin.close();
-		return true;
-	}
-
-	void createFile(const std::string & targetUrl)
-	{
-		// 确保路径存在
-		insurePathExist(getPathOfUrl(targetUrl));
-
-		std::ofstream fout(targetUrl);
-		fout.close();
-	}
-
-	const bool removeFile(const std::string & targetUrl)
-	{
-		try
-		{
-			return boost::filesystem::remove(targetUrl);
-		}
-		catch (std::exception e)
-		{
-			return false;
-		}
-	}
-
-	std::string getPathOfUrl(const std::string & targetUrl)
-	{
-		if (targetUrl.find_last_of("\\/") == -1)
-		{
-			return StringUtils::EmptyString;
-		}
-		else
-		{
-			return std::string(targetUrl.cbegin(), targetUrl.cbegin() + targetUrl.find_last_of("\\/"));
-		}
-	}
-
-	std::string getFileNameOfUrl(const std::string & targetUrl)
-	{
-		return std::string(targetUrl.cbegin() + targetUrl.find_last_of("\\/") + 1, targetUrl.cend());
-	}
-
-	std::string getFileMainNameOfUrl(const std::string & targetUrl)
-	{
-		return std::string(targetUrl.cbegin() + targetUrl.find_last_of("\\/") + 1, 
-			targetUrl.cbegin() + targetUrl.find_last_of(','));
-	}
-
-	std::string getFileExtensionOfUrl(const std::string & targetUrl)
-	{
-		return std::string(targetUrl.cbegin() + targetUrl.find_last_of('.') + 1, targetUrl.cend());
-	}
-
-	std::vector<std::string> getAllFilesUrlsOfPath(const std::string & targetPath)
-	{
-		using namespace boost::filesystem;
-
-		path fullPath(system_complete(path(targetPath, native)));
-		std::vector<std::string> fileNames;
-		if (!exists(fullPath))
-		{
-			insurePathExist(targetPath);
-			return {};
-		}
-		else
-		{
-			for (directory_iterator bgIt(fullPath), endIt; bgIt != endIt; ++bgIt)
-			{
-				if (is_regular_file(bgIt->status()))
-				{
-					fileNames.push_back(bgIt->path().string());
-				}
-			}
-		}
-		return fileNames;
-	}
-
-	std::vector<std::string> getAllDirectoryPathsOfPath(const std::string & targetPath)
-	{
-		using namespace boost::filesystem;
-
-		path fullPath(system_complete(path(targetPath, native)));
-		std::vector<std::string> pathNames;
-		if (!exists(fullPath))
-		{
-			return {};
-		}
-		else
-		{
-			for (directory_iterator bgIt(fullPath), endIt; bgIt != endIt; ++bgIt)
-			{
-				if (is_directory(bgIt->status()))
-				{
-					pathNames.push_back(bgIt->path().string());
-				}
-			}
-		}
-		return pathNames;
-	}
-
-	std::vector<unsigned char> loadFile(const std::string & targetUrl)
-	{
-		if (checkFileExist(targetUrl))
-		{
-			std::ifstream fin(targetUrl, std::ifstream::binary);
-			std::istreambuf_iterator<char> bgIt(fin), edIt;
-			std::vector<unsigned char> data(bgIt, edIt);
-			fin.close();
-			return data;
-		}
-		else
-		{
-			return {};
-		}
-	}
-
-	std::vector<std::string> loadFileByLine(const std::string & targetUrl)
-	{
-		if (checkFileExist(targetUrl))
+		const bool checkFileExist(const std::string & targetUrl)
 		{
 			std::ifstream fin(targetUrl);
-			std::string line;
-			std::vector<std::string> ret;
-
-			while (std::getline(fin, line))
+			if (!fin.is_open())
 			{
-				ret.push_back(line);
+				return false;
 			}
-			return ret;
+
+			fin.close();
+			return true;
 		}
-		else
+
+		void createFile(const std::string & targetUrl)
 		{
-			return {};
+			// 确保路径存在
+			insurePathExist(getPathOfUrl(targetUrl));
+
+			std::ofstream fout(targetUrl);
+			fout.close();
 		}
-	}
 
-	void saveFile(const std::string & targetUrl, const DataUtils::Data& fileData)
-	{
-		insurePathExist(getPathOfUrl(targetUrl));
-
-		_saveFile(targetUrl, fileData.cbegin(), fileData.cend());
-	}
-
-	void saveFile(const std::string & targetUrl, const std::string & fileData)
-	{
-		insurePathExist(getPathOfUrl(targetUrl));
-
-		_saveFile(targetUrl, fileData.cbegin(), fileData.cend());
-	}
-
-	const bool checkPathExist(const std::string & targetPath)
-	{
-		using namespace boost::filesystem;
-
-		path fullPath(system_complete(path(targetPath, native)));
-		return exists(fullPath);
-	}
-
-	const bool insurePathExist(const std::string & targetPath)
-	{
-		using namespace boost::filesystem;
-
-		if (!targetPath.empty())
+		const bool removeFile(const std::string & targetUrl)
 		{
+			try
+			{
+				return boost::filesystem::remove(targetUrl);
+			}
+			catch (std::exception e)
+			{
+				return false;
+			}
+		}
+
+		std::string getPathOfUrl(const std::string & targetUrl)
+		{
+			if (targetUrl.find_last_of("\\/") == -1)
+			{
+				return std::string("");
+			}
+			else
+			{
+				return std::string(targetUrl.cbegin(), targetUrl.cbegin() + targetUrl.find_last_of("\\/"));
+			}
+		}
+
+		std::string getFileNameOfUrl(const std::string & targetUrl)
+		{
+			return std::string(targetUrl.cbegin() + targetUrl.find_last_of("\\/") + 1, targetUrl.cend());
+		}
+
+		std::string getFileMainNameOfUrl(const std::string & targetUrl)
+		{
+			int fileNameBgIndex(targetUrl.find_last_of("\\/"));
+			int fileExtensionBgIndex(targetUrl.find_last_of('.'));
+			if (fileNameBgIndex > fileExtensionBgIndex)
+			{
+				return std::string("");
+			}
+			else
+			{
+				return std::string(targetUrl.cbegin() + fileNameBgIndex + 1, targetUrl.cbegin() + fileExtensionBgIndex);
+			}
+		}
+
+		std::string getFileExtensionOfUrl(const std::string & targetUrl)
+		{
+			return std::string(targetUrl.cbegin() + targetUrl.find_last_of('.') + 1, targetUrl.cend());
+		}
+
+		std::vector<std::string> getAllFilesUrlsOfPath(const std::string & targetPath)
+		{
+			using namespace boost::filesystem;
+
 			path fullPath(system_complete(path(targetPath, native)));
-
+			std::vector<std::string> fileNames;
 			if (!exists(fullPath))
 			{
-				// 创建多层子目录
-				bool ret = create_directories(fullPath);
-				if (ret == false)
+				insurePathExist(targetPath);
+				return {};
+			}
+			else
+			{
+				for (directory_iterator bgIt(fullPath), endIt; bgIt != endIt; ++bgIt)
 				{
-					/*
-					std::ostringstream sout;
-					sout << "无法创建目录: " << fullPath << std::endl;
-					throw std::runtime_error(sout.str().c_str());
-					*/
-
-					return false;
+					if (is_regular_file(bgIt->status()))
+					{
+						fileNames.push_back(bgIt->path().string());
+					}
 				}
+			}
+			return fileNames;
+		}
+
+		std::vector<std::string> getAllDirectoryPathsOfPath(const std::string & targetPath)
+		{
+			using namespace boost::filesystem;
+
+			path fullPath(system_complete(path(targetPath, native)));
+			std::vector<std::string> pathNames;
+			if (!exists(fullPath))
+			{
+				return {};
+			}
+			else
+			{
+				for (directory_iterator bgIt(fullPath), endIt; bgIt != endIt; ++bgIt)
+				{
+					if (is_directory(bgIt->status()))
+					{
+						pathNames.push_back(bgIt->path().string());
+					}
+				}
+			}
+			return pathNames;
+		}
+
+		std::vector<unsigned char> loadFile(const std::string & targetUrl)
+		{
+			if (checkFileExist(targetUrl))
+			{
+				std::ifstream fin(targetUrl, std::ifstream::binary);
+				std::istreambuf_iterator<char> bgIt(fin), edIt;
+				std::vector<unsigned char> data(bgIt, edIt);
+				fin.close();
+				return data;
+			}
+			else
+			{
+				return {};
 			}
 		}
 
-		return true;
-	}
-
-	const bool removePath(const std::string & targetPath)
-	{
-		try
+		std::vector<std::string> loadFileByLine(const std::string & targetUrl)
 		{
-			return boost::filesystem::remove_all(targetPath);
+			if (checkFileExist(targetUrl))
+			{
+				std::ifstream fin(targetUrl);
+				std::string line;
+				std::vector<std::string> ret;
+
+				while (std::getline(fin, line))
+				{
+					ret.push_back(line);
+				}
+				return ret;
+			}
+			else
+			{
+				return {};
+			}
 		}
-		catch (std::exception e)
+
+		void saveFile(const std::string & targetUrl, const Block& fileData)
 		{
-			return false;
+			static const FileSaver<byte> saver;
+			insurePathExist(getPathOfUrl(targetUrl));
+			saver(targetUrl, fileData);
 		}
-	}
 
-	std::string getSystemNativePath(const std::string & targetPath)
-	{
-		using namespace boost::filesystem;
+		void saveFile(const std::string & targetUrl, const std::string & fileData)
+		{
+			static const FileSaver<char> saver;
+			insurePathExist(getPathOfUrl(targetUrl));
+			saver(targetUrl, fileData);
+		}
 
-		return system_complete(path(targetPath, native)).string();
-	}
+		const bool checkPathExist(const std::string & targetPath)
+		{
+			using namespace boost::filesystem;
+
+			path fullPath(system_complete(path(targetPath, native)));
+			return exists(fullPath);
+		}
+
+		const bool insurePathExist(const std::string & targetPath)
+		{
+			using namespace boost::filesystem;
+
+			if (!targetPath.empty())
+			{
+				path fullPath(system_complete(path(targetPath, native)));
+
+				if (!exists(fullPath))
+				{
+					// 创建多层子目录
+					bool ret = create_directories(fullPath);
+					if (ret == false)
+					{
+						/*
+						std::ostringstream sout;
+						sout << "无法创建目录: " << fullPath << std::endl;
+						throw std::runtime_error(sout.str().c_str());
+						*/
+
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		const bool removePath(const std::string & targetPath)
+		{
+			try
+			{
+				return boost::filesystem::remove_all(targetPath);
+			}
+			catch (std::exception e)
+			{
+				return false;
+			}
+		}
+
+		std::string getSystemNativePath(const std::string & targetPath)
+		{
+			using namespace boost::filesystem;
+
+			return system_complete(path(targetPath, native)).string();
+		}
+	};
 };
--- PublicModule/FileUtils.h
+++ PublicModule/FileUtils.h
@@ -1,52 +1,112 @@
 #pragma once
 
-#include <vector>
+#include "Global.h"
+#include "DataUtils/Translator.h"
 #include <string>
 #include <iterator>
 #include <fstream>
 #include <algorithm>
+#include <type_traits>
 
-#include "DataUtils.h"
-
-namespace FileUtils
+namespace SSUtils
 {
-#ifdef _WIN32
-	static const std::string PathSeperator("\\");
-#else
-	static const std::string PathSeperator("/");
-#endif
-
-	const std::string &initailPath();
-
-	const bool checkFileExist(const std::string &targetUrl);
-	void createFile(const std::string &targetUrl);
-	const bool removeFile(const std::string &targetUrl);
-
-	std::string getPathOfUrl(const std::string &targetUrl);
-	std::string getFileNameOfUrl(const std::string &targetUrl);
-	std::string getFileMainNameOfUrl(const std::string &targetUrl);
-	std::string getFileExtensionOfUrl(const std::string &targetUrl);
-	std::vector<std::string> getAllFilesUrlsOfPath(const std::string &targetPath);
-	std::vector<std::string> getAllDirectoryPathsOfPath(const std::string &targetPath);
-	
-	std::vector<unsigned char> loadFile(const std::string &targetUrl);
-	std::vector<std::string> loadFileByLine(const std::string &targetUrl);
-	void saveFile(const std::string &targetUrl, const DataUtils::Data &fileData);
-	void saveFile(const std::string &targetUrl, const std::string &fileData);
-	template<typename iter>
-	void _saveFile(const std::string &targetUrl, const iter bgIt, const iter edIt);
-
-	const bool checkPathExist(const std::string &targetPath);
-	const bool insurePathExist(const std::string &targetPath);
-	const bool removePath(const std::string &targetPath);
-	std::string getSystemNativePath(const std::string &targetPath);
-
-	template<typename iter>
-	void _saveFile(const std::string & targetUrl, const iter bgIt, const iter edIt)
+	namespace File
 	{
-		std::ofstream fout(targetUrl);
-		std::ostream_iterator<DataUtils::byte> outIt(fout);
-		std::copy(bgIt, edIt, outIt);
-		fout.close();
-	}
+		extern const std::string PathSeperator;
+		extern const std::string ExtensionSeperator;
+		extern const std::string InitailPath;
+
+		const bool checkFileExist(const std::string &targetUrl);
+		void createFile(const std::string &targetUrl);
+		const bool removeFile(const std::string &targetUrl);
+
+		std::string getPathOfUrl(const std::string &targetUrl);
+		std::string getFileNameOfUrl(const std::string &targetUrl);
+		std::string getFileMainNameOfUrl(const std::string &targetUrl);
+		std::string getFileExtensionOfUrl(const std::string &targetUrl);
+		std::vector<std::string> getAllFilesUrlsOfPath(const std::string &targetPath);
+		std::vector<std::string> getAllDirectoryPathsOfPath(const std::string &targetPath);
+
+		std::vector<unsigned char> loadFile(const std::string &targetUrl);
+		std::vector<std::string> loadFileByLine(const std::string &targetUrl);
+
+		template<typename T, uint32 DataLength = sizeof(T)>
+		struct FileSaver
+		{
+			FileSaver(void) = default;
+			FileSaver(const FileSaver &ano) = delete;
+			FileSaver(FileSaver &&ano) = delete;
+			FileSaver &operator=(const FileSaver &rhs) = delete;
+			FileSaver &operator=(FileSaver &&rhs) = delete;
+			~FileSaver(void) = default;
+
+			template<typename iter>
+			void operator()(const std::string &targetUrl, const iter bgIt, const iter edIt) const
+			{
+				static_assert(std::is_same_v<iter::value_type, T>, "FileSaver::operator(), the value type of iterator is not the same of the type");
+
+				static const Data::Translator<T> translator;
+				static const FileSaver<byte> saver;
+				Block buff(translator.fromDataIterator(bgIt, edIt));
+				saver(targetUrl, buff.cbegin(), buff.cend());
+			}
+			template<typename container>
+			void operator()(const std::string &targetUrl, const container &datas) const
+			{
+				static_assert(std::is_same_v<container::value_type, T>, "FileSaver::operator(), the value type of container is not the same of the type");
+
+				operator()(targetUrl, datas.cbegin(), datas.cend());
+			}
+		};
+		template<typename T>
+		struct FileSaver<T, 1>
+		{
+			FileSaver(void) = default;
+			FileSaver(const FileSaver &ano) = delete;
+			FileSaver(FileSaver &&ano) = delete;
+			FileSaver &operator=(const FileSaver &rhs) = delete;
+			FileSaver &operator=(FileSaver &&rhs) = delete;
+			~FileSaver(void) = default;
+
+			template<typename iter>
+			void operator()(const std::string &targetUrl, const iter bgIt, const iter edIt) const
+			{
+				static_assert(std::is_same_v<iter::value_type, T>, "FileSaver::operator(), the value type of iterator is not the same of the type");
+
+				std::ofstream fout(targetUrl);
+				std::ostream_iterator<T> outIt(fout);
+				std::copy(bgIt, edIt, outIt);
+				fout.close();
+			}
+			template<typename container>
+			void operator()(const std::string &targetUrl, const container &datas) const
+			{
+				static_assert(std::is_same_v<container::value_type, T>, "FileSaver::operator(), the value type of container is not the same of the type");
+
+				std::ofstream fout(targetUrl);
+				std::ostream_iterator<T> outIt(fout);
+				std::copy(datas.cbegin(), datas.cend(), outIt);
+				fout.close();
+			}
+		};
+		void saveFile(const std::string &targetUrl, const Block &fileData);
+		void saveFile(const std::string &targetUrl, const std::string &fileData);
+		template<typename iter>
+		void saveFile(const std::string &targetUrl, const iter bgIt, const iter edIt)
+		{
+			static const FileSaver<iter::value_type> saver;
+			saver(targetUrl, bgIt, edIt);
+		}
+		template<typename container>
+		void saveFile(const std::string &targetUrl, const container &fileData)
+		{
+			static const FileSaver<container::value_type> saver;
+			saver(targetUrl, fileData);
+		}
+
+		const bool checkPathExist(const std::string &targetPath);
+		const bool insurePathExist(const std::string &targetPath);
+		const bool removePath(const std::string &targetPath);
+		std::string getSystemNativePath(const std::string &targetPath);
+	};
 };
